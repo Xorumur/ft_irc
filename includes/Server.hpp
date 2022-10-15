80b3b40afeab02dbfa3cf4b4f2b8041123a3a3d2
Mathieu <mlecherb@c4r2p4.42nice.fr>
2022-10-15 22:28:56 +0200

    Server cleaned

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -162,28 +162,28 @@ class	Server {
 				FD_SET(sfd, &readfds);  
 				max_sd = sfd;  
 				//add child sockets to set 
-				for ( i = 0 ; i < max_clients ; i++)  
-				{  
-					//socket descriptor 
-					sd = client_socket[i];  
+				// for ( i = 0 ; i < max_clients ; i++)  
+				// {  
+				// 	//socket descriptor 
+				// 	sd = client_socket[i];  
 						
-					//if valid socket descriptor then add to read list 
-					if(sd > 0)  
-						FD_SET( sd , &readfds);  
+				// 	//if valid socket descriptor then add to read list 
+				// 	if(sd > 0)  
+				// 		FD_SET( sd , &readfds);  
 						
-					//highest file descriptor number, need it for the select function 
-					if(sd > max_sd)  
-						max_sd = sd;  
-				} 
+				// 	//highest file descriptor number, need it for the select function 
+				// 	if(sd > max_sd)  
+				// 		max_sd = sd;  
+				// } 
 
-				// for (size_t i = 0; i < user.size(); i++) {
-				// 	sd = user[i]->getFd();
-				// 	std::cout << "test" << std::endl;
-				// 	if (sd > 0)
-				// 		FD_SET(sd, &readfds);
-				// 	if (sd > max_sd)
-				// 		max_sd = sd;
-				// }
+				for (size_t i = 0; i < user.size(); i++) {
+					sd = user[i]->getFd();
+					std::cout << "test" << std::endl;
+					if (sd > 0)
+						FD_SET(sd, &readfds);
+					if (sd > max_sd)
+						max_sd = sd;
+				}
 				std::cout << "Max_sd = " << max_sd << std::endl;
 				//wait for an activity on one of the sockets , timeout is NULL , 
 				//so wait indefinitely
@@ -194,73 +194,12 @@ class	Server {
 				//If something happened on the master socket , 
 				//then its an incoming connection 
 				if (FD_ISSET(sfd, &readfds))  
-				{ 
 					addClient();
-						
-					//add new socket to array of sockets 
-					for (i = 0; i < max_clients; i++)  
-					{  
-						//if position is empty 
-						if( client_socket[i] == 0 )  
-						{  
-							client_socket[i] = fresh_fd;  
-							printf("Adding to list of sockets as %d\n" , i);  
-								
-							break;  
-						}  
-					} 
-					// for (size_t i = 0; i < user.size(); i++) {
-					// 	if (user[i]->getFd() == 0) {
-
-					// 	}
-					// }
-				}  
-					
-				//else its some IO operation on some other socket
-				// for (i = 0; i < max_clients; i++)  
-				// {  
-				// 	sd = client_socket[i];  
-				// 	std::cout << "SD : " << sd << std::endl; 
-				// 	if (FD_ISSET( sd , &readfds))  
-				// 	{  
-				// 		//Check if it was for closing , and also read the 
-				// 		//incoming message 
-						
-				// 		// if ((valread = read( sd , buffer, 1024)) == 0) 
-				// 		if ((valread = recv(sd, buffer, 1024, 0)) == 0)
-				// 		{  
-				// 			//Somebody disconnected , get his details and print 
-				// 			getpeername(sd , (struct sockaddr*)&address , \
-				// 				(socklen_t*)&addrlen);  
-				// 			printf("Host disconnected , ip %s , port %d \n" , 
-				// 				inet_ntoa(address.sin_addr) , ntohs(address.sin_port));  
-								
-				// 			//Close the socket and mark as 0 in list for reuse 
-				// 			close( sd );  
-				// 			client_socket[i] = 0;  
-				// 		}  
-
-				// 		//Echo back the message that came in 
-				// 		else 
-				// 		{  
-				// 			//set the string terminating NULL byte on the end 
-				// 			//of the data read 
-				// 			buffer[valread] = '\0';
-				// 			// for (size_t i = 0; i < serv.user.size(); i++)
-				// 			// 	send(serv.user[i]->getFd(), buffer, strlen(buffer), 0);
-				// 			parse parse(buffer);
-				// 			// parse.exec(*this);
-				// 			// std::cout << buffer << "    |" << std::endl;
-				// 			send(sd , buffer , strlen(buffer) , 0 );  
-				// 		}  
-				// 	}  
-				// }
-				// std::cout << "Size client : " << user.size() << std::endl;
 				for (size_t i = 0; i < user.size(); i++) {
 					sd = user[i]->getFd();
-					std::cout << "SD : " << sd << std::endl;
+					// std::cout << "SD : " << sd << std::endl;
 					if (FD_ISSET(sd, &readfds)) {
-						std::cout << "ici" << std::endl;
+						// std::cout << "ici" << std::endl;
 						if ((valread = recv(sd, buffer, 1024, 0)) == 0)
 						{  
 							//Somebody disconnected , get his details and print 
@@ -276,7 +215,7 @@ class	Server {
 						}
 			
 						else {
-							std::cout << "la" << std::endl;
+							// std::cout << "la" << std::endl;
 							buffer[valread] = '\0';
 							std::cout << buffer << std::endl;
 							// parse parse(buffer);
